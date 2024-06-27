a198573bfdf990a6ad2485174292c3d467366cfa
ãs3496h <s3496h@gmail.com>
2024-06-27 13:39:35 +0900

    별찍기

---
+++ src/main/java/org/koreait/Main.java
@@ -0,0 +1,16 @@
+package org.koreait;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        for (int i = 1; i < a ; i++ )
+
+            System.out.println();
+
+    }
+
+}
